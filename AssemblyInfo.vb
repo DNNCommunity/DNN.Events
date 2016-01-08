59d2e1341cbcfa4f9920c9bb2afc2a77bfe1e12a
George Alatrash <george.alatrash@dnnsoftware.com>
2016-01-08 08:11:02 -0800

    CONTENT-5784: upgraded to .NET 4.5 to be able to reference deprecated Telerik related controls in DNN 8.0.0.

--- AssemblyInfo.vb
+++ AssemblyInfo.vb
@@ -48,5 +48,5 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("6.2.1.0")> 
-<Assembly: AssemblyFileVersionAttribute("6.2.1.0")> 
+<Assembly: AssemblyVersion("6.3.0.0")> 
+<Assembly: AssemblyFileVersionAttribute("6.3.0.0")> 
